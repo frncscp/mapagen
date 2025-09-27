7ecb451397293ac6ca00625f586e11cf9cb19e46
frncscp <franc@atman.org>
2025-09-26 22:55:00 -0500

    fix: add geojson suffix

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ st.set_page_config(
 
 st.title("mapagen")
 
-mapa = st.file_uploader(label = "Sube el mapa a renderizar", type = "geo.json")
+mapa = st.file_uploader(label = "Sube el mapa a renderizar", type = ["geo.json", "geojson"])
 st.divider()
 loadMap(mapa)
